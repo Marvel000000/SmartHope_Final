309ba70e9b3c4bce6ccc33a41c92c13306bccdef
Levram <89078633+Marvel000000@users.noreply.github.com>
2023-06-15 22:52:31 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let body = document.body;
+
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   profile.classList.remove('active');
+}
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+
+document.querySelectorAll('input[type="number"]').forEach(InputNumber => {
+   InputNumber.oninput = () =>{
+      if(InputNumber.value.length > InputNumber.maxLength) InputNumber.value = InputNumber.value.slice(0, InputNumber.maxLength);
+   }
+});
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   searchForm.classList.remove('active');
+
+   if(window.innerWidth < 1200){
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
+
+}
+
---
+++ js/script2.js
@@ -0,0 +1,34 @@
+let body = document.body;
+
+let profile = document.querySelector('.header .flex .profile');
+
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   profile.classList.remove('active');
+}
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+
+toggleBtn.onclick = (e) =>{
+   let darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enabelDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
+
